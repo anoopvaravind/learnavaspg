dda118c5142a12d4a85543a7ed3bacc512e6f453
Anoop <a4anooparavind@gmail.com>
2017-03-15 08:35:48 +0530

    Adding repo

---
+++ expensemanager/src/main/resources/db_script.sql
@@ -0,0 +1,12 @@
+
+# Category insert
+insert into category values(1,'Food','Food');
+insert into category values(2,'Electricity','Electricity');
+insert into category values(3,'Internet','Internet');
+insert into category values(4,'Gas','Gas');
+insert into category values(5,'Water','Water');
+insert into category values(6,'Newspaper','Newspaper');
+insert into category values(7,'Maintanence','Maintanence');
+insert into category values(8,'New Item Purchase','New Item Purchase');
+insert into category values(9,'Other','Other');
+
