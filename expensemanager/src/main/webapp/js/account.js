f21f3cc0b589519228b1a379519d03f2cdf41890
anoopvaravind <anooparavindmbit@gmail.com>
2017-03-16 14:28:34 +0530

    Create account.js

---
+++ expensemanager/src/main/webapp/js/account.js
@@ -0,0 +1,53 @@
+
+//csrf tokens for ajax post
+var token = $("meta[name='_csrf']").attr("content");
+var header = $("meta[name='_csrf_header']").attr("content");
+$(document).ready(function(){
+	$("#generate").prop("disabled", false);
+	jQuery.validator.addMethod(
+    "validateMoney",
+    function(value, element) {
+        var isValidMoney = /^\d{0,4}(\.\d{0,2})?$/.test(value);
+        return this.optional(element) || isValidMoney;
+    },
+    "Please enter a valid amount"
+	);
+
+	$("form[name='accountForm']").validate({
+		// Specify validation rules
+		rules: {
+			// The key name on the left side is the name attribute
+			// of an input field. Validation rules are defined
+			// on the right side
+			currentrentamount:{
+				required:true,
+				validateMoney:true
+			}
+		},
+		// Specify validation error messages
+		messages: {
+			currentrentamount:{
+				required:"Please enter rent amount"
+			}
+		},
+		// Make sure the form is submitted to the destination defined
+		// in the "action" attribute of the form when valid
+		submitHandler: function(form) {
+			e.preventDefault(); /// <--- THIS LINE
+    $.ajax({
+        type: 'POST',
+        data: {name: $('#name').val()},
+        url: 'signup_backend.php',
+        success: function(data, textStatus, jqXHR){
+
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            alert('error');
+        }
+    });
+		}
+	});
+
+
+});
+
